b0c1c1b01d0ebaae3dff03cebfc1df5fb0367b21
mgcarrol <37454587+mgcarrol@users.noreply.github.com>
2019-01-14 18:08:02 -0500

    added responsive features

---
+++ script.js
@@ -0,0 +1,48 @@
+let navHome = document.getElementById('home');
+let navAbout = document.getElementById('about');
+let navProjects = document.getElementById('projects');
+let navExperiences = document.getElementById('experiences');
+let navCoursework = document.getElementById('coursework');
+navHome.onmouseover = function(){
+  navHome.style.textDecoration = 'underline';
+  navHome.style.color = '#1fc2d3';
+};
+navHome.onmouseout = function() {
+  navHome.style.textDecoration = '';
+  navHome.style.color = '';
+};
+navAbout.onmouseover = function(){
+  navAbout.style.textDecoration = 'underline';
+  navAbout.style.color = '#1fc2d3';
+};
+navAbout.onmouseout = function() {
+  navAbout.style.textDecoration = '';
+  navAbout.style.color = '';
+};
+navProjects.onmouseover = function(){
+  navProjects.style.textDecoration = 'underline';
+  navProjects.style.color = '#1fc2d3';
+};
+navProjects.onmouseout = function() {
+  navProjects.style.textDecoration = '';
+  navProjects.style.color = '';
+};
+navExperiences.onmouseover = function(){
+  navExperiences.style.textDecoration = 'underline';
+  navExperiences.style.color = '#1fc2d3';
+};
+navExperiences.onmouseout = function() {
+  navExperiences.style.textDecoration = '';
+  navExperiences.style.color = '';
+};
+navCoursework.onmouseover = function(){
+  navCoursework.style.textDecoration = 'underline';
+  navCoursework.style.color = '#1fc2d3';
+};
+navCoursework.onmouseout = function() {
+  navCoursework.style.textDecoration = '';
+  navCoursework.style.color = '';
+};
+
+
+// navHome.addEventListener('mouseover', changeNav);
